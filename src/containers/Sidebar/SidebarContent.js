e7c8eb697e0364323e00c298eb908c24ee1cf7ba
noormaya098 <73688185+noormaya098@users.noreply.github.com>
2023-07-12 11:45:23 +0700

    coba
    Co-Authored-By: Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>

--- src/containers/Sidebar/SidebarContent.js
+++ src/containers/Sidebar/SidebarContent.js
@@ -104,7 +104,7 @@ const SidebarContent = ({ sidebarCollapsed, setSidebarCollapsed }) => {
                     onClick={() => createap()}
                     variant="warning"
                     >
-                    ADD AP 
+                    ADD AP Maya
                   </Button>
                 </div>
                     </>
